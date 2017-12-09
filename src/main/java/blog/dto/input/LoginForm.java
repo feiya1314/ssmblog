b26129b78bc2b4c90aebcce8d704b57823196072
feiya1314 <yufei1313@gmail.com>
2017-12-09 20:37:56 +0800

    [add ajax request][master][commit by feiya]

---
+++ src/main/java/blog/dto/input/LoginForm.java
@@ -0,0 +1,53 @@
+package blog.dto.input;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class LoginForm {
+    private String username;
+    private String password;
+    private String currentUrl;
+    private String email;
+    private String phone;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCurrentUrl() {
+        return currentUrl;
+    }
+
+    public void setCurrentUrl(String currentUrl) {
+        this.currentUrl = currentUrl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
